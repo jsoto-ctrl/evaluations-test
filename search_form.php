eec126df3fe7a73867143be1a412d69843f85ff2
jsoto-ctrl <jsoto@iu.edu>
2026-05-06 11:51:08 -0400

    Update search_form.php

--- search_form.php
+++ search_form.php
@@ -15,9 +15,10 @@ include ("css/header.html");
 ?>      
 <p>&nbsp;</p><h2>Student Evaluations: Search</h2>
 <div class="content"><form method="post" action="search_results.php">
+<h3>TESTING FROM BBEDIT-See here</h3>
 <table width="100%" border="0" cellspacing="0" cellpadding="5">
   <tr>
-	<td colspan="2"><p>TESTING FROM BBEDIT: Type in the last name you wish to look up. You can use wildcards to expand your search. The percent sign acts like a wildcard. Think of it as having the meaning of <i>whatever.</i></p>
+	<td colspan="2"><p> Type in the last name you wish to look up. You can use wildcards to expand your search. The percent sign acts like a wildcard. Think of it as having the meaning of <i>whatever.</i></p>
 	<hr><h4>Search examples</h4>
 	
 	<p>Strict search: Enter "Soto" and only instructors with that exact last name will be retrieved.</p>
