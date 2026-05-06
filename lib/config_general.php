be4ed271b876a7a52ce63331a6e5926ac11d93f4
jsoto-ctrl <jsoto@iu.edu>
2026-05-06 11:20:30 -0400

    Update config_general.php

--- lib/config_general.php
+++ lib/config_general.php
@@ -1,9 +1,6 @@
 <?
-#$servidor = $_ENV['MYSQL_HOST'];
-#$usuario_db = $_ENV['MYSQL_USER'];
-#$contrasenna = $_ENV['MYSQL_PASSWORD'];
+$servidor = $_ENV['MYSQL_HOST'];
+$usuario_db = $_ENV['MYSQL_USER'];
+$contrasenna = $_ENV['MYSQL_PASSWORD'];
 
-$servidor = 'mysql-test.uits.iu.edu:3306';
-$usuario_db = 'ancla_root';
-$contrasenna = '5?cedq%@cFBe]z?T(AV,e?KD-vmSp6';
 ?>
